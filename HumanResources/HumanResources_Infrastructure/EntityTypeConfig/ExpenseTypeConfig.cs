dd62c678384f485e0c6466c9bddba76956659619
pelinadar <114329608+PelinADAR@users.noreply.github.com>
2024-08-01 22:57:26 +0300

    HumanResources

---
+++ HumanResources/HumanResources_Infrastructure/EntityTypeConfig/ExpenseTypeConfig.cs
@@ -0,0 +1,25 @@
+﻿using HumanResources_Domain.Entities;
+using HumanResources_Domain.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources_Infrastructure.EntityTypeConfig
+{
+    internal class ExpenseTypeConfig : BaseEntityConfig<ExpenseType>
+    {
+        public override void Configure(EntityTypeBuilder<ExpenseType> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.ExpenseTypeName)
+               .IsRequired(true)
+               .HasMaxLength(50);
+
+            base.Configure(builder);
+        }
+    }
+}
---
+++ HumanResources/HumanResources_Infrastructure/EntityTypeConfig/LeaveTypeConfig.cs
@@ -0,0 +1,29 @@
+﻿using HumanResources_Domain.Entities;
+using HumanResources_Domain.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources_Infrastructure.EntityTypeConfig
+{
+    internal class LeaveTypeConfig : BaseEntityConfig<LeaveType>
+    {
+        public override void Configure(EntityTypeBuilder<LeaveType> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.LeaveTypeName)
+               .IsRequired(true)
+               .HasMaxLength(50);
+
+            builder.Property(x => x.DefaultDays)
+               .IsRequired(true);
+
+            base.Configure(builder);
+        }
+    }
+}
+
